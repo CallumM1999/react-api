09300de3f1661d96a19f31018f8bdcd7723aecee
cal <callummac@protonmail.com>
2018-11-11 20:38:07 +0000

    Changed port to process.env.PORT

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const PORT = process.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
