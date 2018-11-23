09d1541701e48f4300fdd50cd5489520aca6205a
cal <callummac@protonmail.com>
2018-11-23 13:04:15 +0000

    Added transporter auth to seperate file

--- server/mail/transporter.js
+++ server/mail/transporter.js
@@ -1,13 +1,12 @@
 const nodemailer = require('nodemailer');
 
+const auth = require('../../conf.json');
+
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     secure: false,
     port: 25,
-    auth: {
-        user: 'callum.macpherson.apps@gmail.com',
-        pass: 'M52YsWT$!W1KCPshF^LwgSjuCKlnQO09bwxZLzN!5$N@iw*HFcjqHGWthfv*!^b1rSqKZ12fKSvxYLAQasMts12bE##Oc9vF^01Q'
-    },
+    auth,
     tls: {
         rejectUnauthorized: false
     }
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+
 app.use(cors());
 
 const mongoose = require('mongoose');
