0510ab3ec9bdf314be65636f539c45652e5e3ede
cal <callummac@protonmail.com>
2018-11-23 13:38:54 +0000

    logging errors

--- server/mail/methods/sendEmail.js
+++ server/mail/methods/sendEmail.js
@@ -1,8 +1,14 @@
 const transporter = require('../transporter');
 
+
+
 const sendEmail = template => {
     return new Promise((resolve, reject) => {
         transporter.sendMail(template, (error, info) => {
+
+            console.log('sendemail', error);
+            console.log('sendemail', info);
+
             if (error) reject(new Error(error));
             resolve(info);
         });
