684775b55f4fa5aa60e6c3987e98cf23c548065c
cal <callummac@protonmail.com>
2018-11-28 11:54:56 +0000

    Didn't get submitted for last submit.

--- server/generateToken.js
+++ server/generateToken.js
@@ -1,5 +1,5 @@
-const jwt_key = 'secret';
 const jwt = require('jsonwebtoken');
+const jwt_key = process.env.TOKEN_SECRET || 'oN6nuiwcOVW06GbLwKWaCH5ohap9ieMlxwEb6zENFeurbAVhGBD74DQekaDbGf4vCZfPVM6tZkBtiCdiww2Rb34qq7CkEQoqF8y5';
 
 const generateToken = data => {
     return jwt.sign(data, jwt_key, { expiresIn: '1hour' });
